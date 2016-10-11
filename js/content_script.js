a9394844b2fd9b21eda9979dd00dd11280f2c53c
Zhiqiang Ye <xiqian013@live.com>
2016-10-11 18:24:42 +0800

    重大改进

--- js/content_script.js
+++ js/content_script.js
@@ -1,10 +1,3 @@
-/*************************************************************************
-    > File Name: js/content_scripts.js
-    > Author: yq
-    > Mail: xiqian013@live.com 
-    > Created Time: 2015-11-3 22:57:15
- ************************************************************************/
-
 'use strict';
 function UnHookEvent(onevent) {
 	var _event = onevent.substr(2);
@@ -20,22 +13,8 @@ function UnHookEventCopy(onevent) {
 	}, true);
 }
 
-function modifyTitle(){
-    var array = ['1','2','4'];
-    for(var i = 0 ; i < array.length ; i++){
-        var div = document.getElementsByClassName('main-left' + array[i]);
-        for (var j = 0 ; j < div.length ; j++){
-            var li = div[j].getElementsByTagName('li');
-            for (var k = 0 ; k < li.length ; k++){
-                var a = li[k].getElementsByTagName('a')[0];
-                a.text=a.title.substr(0,18);
-            }
-        }
-    }
-}
 window.onload = function(){
     UnHookEvent("oncontextmenu");
     UnHookEvent("onselectstart");
     UnHookEventCopy("oncopy");
-    modifyTitle();
 };
---
+++ js/popup.js
@@ -0,0 +1,50 @@
+﻿//'use strict'
+var feedUrl = "http://www.quicy.cn/rss/nku_math/all.xml";
+var req;
+var maxFeedItems = 8;
+
+function main() {
+    req = new XMLHttpRequest();
+    req.onload = handleResponse;
+    req.onerror = handleError;
+    req.open('GET', feedUrl, true);
+    req.send(null);
+}
+
+function handleFeedParsingFailed(error) {
+    var feed = document.getElementById('feed');
+    feed.className = 'error';
+    feed.innerText = 'Error: ' + error;
+}
+
+function handleError() {
+    handleFeedParsingFailed('Failed to fetch RSS feed.');
+}
+
+function handleResponse() {
+    var doc = req.responseXML;
+    if (!doc) {
+        handleFeedParsingFailed('Not a valid feed.');
+        return;
+    }
+    buildPreview(doc);
+}
+
+function buildPreview(doc) {
+    var items = doc.getElementsByTagName('item');
+    var html = "";
+    for (var i = 0 ; i < maxFeedItems ; i++) {
+        var item = items[i];
+        if (item !== undefined){
+            var title = item.getElementsByTagName('title')[0].textContent;
+            var url = item.getElementsByTagName('link')[0].textContent;
+            var date = item.getElementsByTagName('date')[0].textContent;
+            html += "<li>";
+            html += "<a href='" + url + "' target='_blank'>"+title+"---"+date+"</a>";
+            html += "</li><hr/>";
+        }
+    }
+    document.getElementById('list').innerHTML = html;
+}
+
+window.onload = main;
